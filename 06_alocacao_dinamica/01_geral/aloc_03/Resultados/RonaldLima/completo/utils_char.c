1972c0107fddc3ea4287e68c94669da74c481f02
capixabasss <ronald.alvxs@gmail.com>
2024-12-19 17:55:56 +0000

    Alocacao dinamica 03 - Incompleto

---
+++ 06_alocacao_dinamica/01_geral/aloc_03/Resultados/RonaldLima/completo/utils_char.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include "utils_char.h"
+
+char *CriaVetor(int tamanho) {
+    int i;
+    char *vetor;
+    vetor = malloc((tamanho+1)*sizeof(char));
+    assert(vetor);
+
+    for(i = 0; i < tamanho; i++) {
+        vetor[i] = '_';
+    }
+    *(vetor+i) = '_';
+    
+    return vetor;
+}
+
+void LeVetor(char *vetor, int tamanho) {
+    int i;
+    char c;
+
+    for(i = 0; i < tamanho; i++) {
+        scanf("%c", &c);
+        if(c == '\n') break;
+        vetor[i] = c;
+    }
+}
+
+void ImprimeString(char *vetor, int tamanho) {
+    int i;
+    for(i = 0; i < tamanho; i++) {
+        printf("%c", vetor[i]);
+    }
+    printf("\n");
+}
+
+void LiberaVetor(char *vetor) {
+    free(vetor);
+}
